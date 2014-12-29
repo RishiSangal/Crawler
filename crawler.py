8f1f534bd424241db193edb225fac15b40f92b17
Lavish Aggarwal <lucky.lavish@gmail.com>
2014-12-29 22:49:02 +0530

    url checking code optimized

--- crawler.py
+++ crawler.py
@@ -1,34 +1,38 @@
-import urllib2
-import httplib
-import urlparse
+#import urllib2
+#import httplib
+#import urlparse
 
-def get_server_status_code(url):
-    """
-    Download just the header of a URL and
-    return the server's status code.
-    """
-    host, path = urlparse.urlparse(url)[1:3]    # elems [1] and [2]
-    try:
-        conn = httplib.HTTPConnection(host)
-        conn.request('HEAD', path)
-        return conn.getresponse().status
-    except StandardError:
-        return None
+#def get_server_status_code(url):
+#    """
+#    Download just the header of a URL and
+#    return the server's status code.
+#    """
+#    host, path = urlparse.urlparse(url)[1:3]    # elems [1] and [2]
+#    try:
+#        conn = httplib.HTTPConnection(host)
+#        conn.request('HEAD', path)
+#        return conn.getresponse().status
+#    except StandardError:
+#        return None
  
-def check_url(url):
-    """
-    Check if a URL exists without downloading the whole file.
-    We only check the URL header.
-    """
-    good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
-    return get_server_status_code(url) in good_codes
+#def check_url(url):
+#    """
+#    Check if a URL exists without downloading the whole file.
+#    We only check the URL header.
+#    """
+#    good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
+#    return get_server_status_code(url) in good_codes
 
 def get_page(url):
-	if check_url(url):
-		seed = urllib2.urlopen(url)
-		page = seed.read()
-		return page
-	return ""
+#	if check_url(url):
+#		seed = urllib2.urlopen(url)
+#		page = seed.read()
+#		return page
+	try:
+		import urllib2
+		return urllib2.urlopen(url).read()
+	except:
+		return ""
 
 
 def get_next_link(page):
@@ -46,7 +50,7 @@ def get_all_links(page):
 		url,end_quotes = get_next_link(page)
 		if not url==None:
 			links.append(url)
-			#print url
+#			print url
 			page = page[end_quotes:]
 		else:
 			return links
@@ -60,17 +64,16 @@ def crawl_web(seed,max_depth):
 	crawled = []
 	next_depth = []
 	depth = 0
-	while tocrawl and depth<=max_depth:
+	while tocrawl and depth<max_depth:
 		page = tocrawl.pop()
 		if page not in crawled:
 			union(next_depth,get_all_links(get_page(page)))
-			print page
 			crawled.append(page)
 		if not tocrawl:
 			tocrawl, next_depth=next_depth, []
 			depth=+1
 
-	return crawled
+	return tocrawl
 
 #seed = 'https://www.udacity.com/cs101x/index.html'
 seed = 'http://www.caranddriver.com/'
