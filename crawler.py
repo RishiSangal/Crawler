046a73a39199611a8b1371b007b0f5b5fec648dc
Lavish Aggarwal <lucky.lavish@gmail.com>
2015-04-04 08:24:48 +0530

    indentation improved and warnings resloved

--- crawler.py
+++ crawler.py
@@ -5,11 +5,13 @@ import pymongo
 
 db = pymongo.Connection(host='128.199.148.204')['crawl']
 
+
 def internet_on():
     try:
-        response=urllib2.urlopen('http://74.125.228.100',timeout=3)
+        response = urllib2.urlopen('http://74.125.228.100', timeout=3)
         return True
-    except urllib2.URLError as err: pass
+    except urllib2.URLError:
+        pass
     return False
 
 
@@ -27,31 +29,33 @@ def get_page(url):
 
 def get_next_link(page):
     start_link = page.find('<a href')
-    if start_link==-1:
+    if start_link == -1:
         return None, 0
-    start_quotes = page.find('"',start_link)
-    end_quotes = page.find('"',start_quotes+1)
+    start_quotes = page.find('"', start_link)
+    end_quotes = page.find('"', start_quotes+1)
     url = page[start_quotes+1:end_quotes]
-    return url,end_quotes
+    return url, end_quotes
+
 
 def get_all_links(page):
-    links=[]
+    links = []
     while True:
-        url,end_quotes = get_next_link(page)
-        if not url ==None:
+        url, end_quotes = get_next_link(page)
+        if not url == None:
             if 'http' in url:
-                if not (('#' or 'facebook')  in url):
+                if not (('#' or 'facebook') in url):
                     links.append(url)
             page = page[end_quotes:]
         else:
             return links
+
 
 def get_all_keywords(page):
     keywords = []
     print "grabbing keywords"
     start_link = page.find('<meta')
     if not start_link == -1:
-        key_meta_start = page.find('keywords',start_link+1)
+        key_meta_start = page.find('keywords', start_link+1)
         if not key_meta_start == -1:
             content_start = page.find('content', key_meta_start+1)
             content_start_quote = page.find('"', content_start)
@@ -67,8 +71,9 @@ def get_all_keywords(page):
     return keywords
 
 
-def store_data(link,keywords):    
+def store_data(link, keywords):
     print "storing data"
+    # ElasticSearch
     # data = {'keywords':keywords}
     # data = json.dumps(data)
     # # r = requests.put('http://128.199.148.204:9200/crawler/1/1',data)
@@ -77,60 +82,59 @@ def store_data(link,keywords):
     # out = urllib2.urlopen(req)
     # print out
 
-    #redis
+    # redis
     # if cRedis.hmget(link,['keywords']) == [None]:
     #     cRedis.hmset(link, {'keywords': keywords})
     # else:
     #     print(cRedis.hmget(link,['keywords']))
 
-    #mongo
+    # mongo
     if not keywords == []:
-        if db.data.find({'url':link}).count() == 0:
+        if db.data.find({'url': link}).count() == 0:
             try:
-
-                db.data.insert({'url':link,'keywords':keywords, 'timestamp': datetime.datetime.now()})
-
+                db.data.insert({'url': link, 'keywords': keywords, 'timestamp': datetime.datetime.now()})
             except:
                 pass
-    #db.newdata.find({},{})
+
 
 def index_fields():
     db.data.ensure_index('url')
     db.data.ensure_index('keywords')
 
-def union(p,q):
+
+def union(p, q):
     for e in q:
         if e not in p:
             p.append(e)
 
 
-def crawl_web(seed,max_depth):
+def crawl_web(seed, max_depth):
     tocrawl = [seed]
     crawled = []
     next_depth = []
     depth = 0
     print "crawling started"
-    while tocrawl and depth<max_depth:
+    while tocrawl and depth < max_depth:
         page = tocrawl.pop()
         if (';' or '&' or '=' or '%') in page:
-            print 'skipped url',page
+            print 'skipped url', page
             continue
         if page not in crawled:
-            union(next_depth,get_all_links(get_page(page)))
+            union(next_depth, get_all_links(get_page(page)))
             keywords = get_all_keywords(get_page(page))
-            store_data(page,keywords)
+            store_data(page, keywords)
             print page
             crawled.append(page)
         if not tocrawl:
-            tocrawl, next_depth=next_depth, []
-            depth=+1
+            tocrawl, next_depth = next_depth, []
+            depth = +1
 
     return tocrawl
 
 
-seed = 'http://amazon.com'
-max_depth = 5 #defining number of webpages to be crawled
-Links = crawl_web(seed,max_depth)
+seed_url = 'http://amazon.com'
+max_depth_url = 5  # defining number of webpages to be crawled
+Links = crawl_web(seed_url, max_depth_url)
 index_fields()
 # print Links
 
